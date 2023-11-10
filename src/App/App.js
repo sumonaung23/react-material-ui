64ae034b3963b3fb176eee507f4d0c6ddfca6089
sumonaung23 <sumonaung575@gmail.com>
2023-11-10 11:42:18 +0630

    created starter folders and files

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import SideMenu from '../components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu />
+      App
+    </div>
+  );
+}
+
+export default App;
